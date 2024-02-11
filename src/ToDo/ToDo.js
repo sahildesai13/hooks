a460068d77ac8e49bb0e14dda20ce08c3f324c9f
sahil desai <133312679+sahildesai13@users.noreply.github.com>
2024-02-11 10:06:38 +0530

    Sahil -ToDo

--- src/ToDo/ToDo.js
+++ src/ToDo/ToDo.js
@@ -1,109 +1,120 @@
-import { useState } from "react";
 import './ToDo.css';
+import { useState } from 'react';
+import { Container, Row, Col } from "react-bootstrap";
+
 function ToDo() {
 
-  const [task, setTask] = useState("");
-  const [todo, setTodo] = useState([]);
-  const [edit, setEdit] = useState(null);
-  const [search, setSearch] = useState("");
-  const [final, setFinal] = useState([]);
-  const [searchResult, setSearchResult] = useState([]);
+    const [task, setTask] = useState("");
+    const [todo, setTodo] = useState([]);
+    const [edit, setEdit] = useState(null);
+    const [search, setSearch] = useState("");
+    const [final, setFinal] = useState([]);
 
-  const add = () => {
-    if (edit !== null) {
-      const updated = [...todo];
-      updated[edit] = { task: task, checked: false };
-      setTodo(updated);
-      setFinal(updated);
-      setEdit(null);
-      setTask("");
-    } else {
-      const updatedTodo = [...todo, { task: task, checked: false }];
-      setTodo(updatedTodo);
-      setFinal(updatedTodo);
-      setTask("");
+    const add = () => {
+        if (edit !== null) {
+            const updated = [...todo];
+            updated[edit] = { task: task, checked: false };
+            setTodo(updated);
+            setFinal(updated);
+            setEdit(null);
+            setTask("");
+        }
+        else {
+            setTodo([...todo, { task: task, checked: false }]);
+            setFinal([...todo, { task: task, checked: false }]);
+            setTask("");
+        }
     }
-  };
 
-  const del = (index) => {
-    const updatedTodo = todo.filter((val, id) => id !== index);
-    setTodo(updatedTodo);
-    setFinal(updatedTodo);
-  };
+    const del = (index) => {
+        let d = todo.filter((val, id) => {
+            return id !== index;
+        })
+        setTodo(d);
+        setFinal(d);
+    }
 
-  const update = (index) => {
-    setEdit(index);
-    setTask(todo[index].task);
-  };
+    const update = (index) => {
+        setEdit(index);
+        setTask(todo[index].task);
+    };
 
-  const handleCheck = (index) => {
-    setTodo((prevTodo) =>
-      prevTodo.map((item, i) =>
-        i === index ? { ...item, checked: !item.checked } : item
-      )
-    );
-  };
+    const handlecheck = (index) => {
+        const check = [...todo];
+        check[index].checked = !check[index].checked;
+        setTodo(check);
+        setFinal(check);
+    }
 
-  const searchHandler = () => {
-    const info = final.filter((val) => val.task === search);
-    setSearchResult(info);
-  };
+    const searchhanlder = () => {
+        let info = todo.filter((val) => {
+            return val.task === search;
+        })
+        setTodo(info);
+    }
 
-  const completed = () => {
-    const com = final.filter((val) => val.checked === true);
-    setTodo(com);
-  };
+    const completed = () => {
+        let com = final.filter((val) => {
+            return val.checked === true
+        });
+        setTodo(com);
+    }
 
-  const uncompleted = () => {
-    const uncom = final.filter((val) => val.checked === false);
-    setTodo(uncom);
-  };
+    const uncompleted = () => {
+        let uncom = final.filter((val) => {
+            return val.checked === false
+        });
+        setTodo(uncom);
+    }
 
-  const all = () => {
-    setTodo([...final]);
-  };
+    const all = () => {
+        var data = [...final];
+        setTodo(data);
+    }
 
-  return (
-    <div className="">
-      <div className="wrapper">
-        <div className='form'>
-          <input type="text" className='input' value={task} placeholder='Enter Task' onChange={(e) => { setTask(e.target.value) }} />
-          <input type='button' className='btn' value={"Add Task"} onClick={() => { add() }} /><br />
-          <input type='text' className='input' placeholder='Search' value={search} onChange={(e) => setSearch(e.target.value)} />
-          <input type='button' className='btn' value={"Search"} onClick={() => { searchHandler() }} /><br />
-          <input type='button' className='btn' value={"Completed"} onClick={() => { completed() }} style={{ marginRight: "10px" }} />
-          <input type='button' className='btn' value={"UnCompleted"} onClick={() => { uncompleted() }} style={{ marginRight: "10px" }} />
-          <input type='button' className='btn' value={"All"} onClick={() => { all() }} style={{ marginRight: "10px" }} />
+    return (
+        <div className="App">
+            <div className="wrapper text-center">
+                <h1>Todo List</h1>
+                <Row>
+                    <Col>
+                        <input type="text" className='input' value={task} placeholder='Enter Task' onChange={(e) => { setTask(e.target.value) }} />
+                        <input type='button' value={"Add Task"} onClick={() => { add() }} /><br />
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <input type='text' className='input' placeholder='Search' value={search} onChange={(e) => setSearch(e.target.value)} />
+                        <input type='button' value={"Search"} onClick={() => { searchhanlder() }} /><br />
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <input type='button' value={"Completed"} onClick={() => { completed() }} />
+                        <input type='button' value={"UnCompleted"} onClick={() => { uncompleted() }} />
+                        <input type='button' value={"All"} onClick={() => { all() }} />
+                    </Col>
+                </Row>
+                <div className='form'>
+
+                </div>
+
+                <ul className='' style={{ marginTop: "10px"}}>
+                    {
+                        todo.map((ele, index) => {
+                            return (
+                                <li className='todo' key={index}>
+                                    <input type='checkbox' checked={ele.checked} onChange={() => handlecheck(index)} />
+                                    <span style={{ textDecoration: ele.checked ? "line-through" : "" }}>{ele.task}</span>
+                                        <input type='button' value={"Del"} className='del' onClick={() => { del(index) }} />
+                                    <input type='button' value={"Edit"} onClick={() => { update(index) }} />
+                                </li>
+                            )
+                        })
+                    }
+                </ul>
+            </div>
         </div>
-
-        <ul className='' style={{ marginTop: "20px" }}>
-          {searchResult.length > 0 ? (
-            searchResult.map((ele, index) => (
-              <li className='' key={index}>
-                <input type='checkbox' checked={ele.checked} onChange={() => handleCheck(index)} />
-                <span style={{ textDecoration: ele.checked ? "line-through" : "" }}>{ele.task}</span>
-                <div className='button'>
-                  <input type='button' value={"Del"} className='del' onClick={() => { del(index) }} />
-                  <input type='button' value={"Edit"} onClick={() => { update(index) }} />
-                </div>
-              </li>
-            ))
-          ) : (
-            todo.map((ele, index) => (
-              <li className='' key={index}>
-                <input type='checkbox' checked={ele.checked} onChange={() => handleCheck(index)} />
-                <span style={{ textDecoration: ele.checked ? "line-through" : "" }}>{ele.task}</span>
-                <div className='button'>
-                  <input type='button' value={"Del"} className='del' onClick={() => { del(index) }} />
-                  <input type='button' value={"Edit"} onClick={() => { update(index) }} />
-                </div>
-              </li>
-            ))
-          )}
-        </ul>
-      </div>
-    </div>
-
-    )
+    );
 }
 export default ToDo;
